366b112aa561d5ee51b233baa96b05fd3bf57e1d
mgundas <mehmetgundass@gmail.com>
2024-02-23 23:21:28 +0300

    Added magenta color option for back-end logger.

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -2,34 +2,37 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const getColor = (fg) => {
-   let fgc = '\x1b[37m'
-   switch (fg.trim().toLowerCase()) {
-     case 'black':
-       fgc = '\x1b[30m'
-       break;
-     case 'red':
-       fgc = '\x1b[31m'
-       break;
-     case 'green':
-       fgc = '\x1b[32m'
-       break;
-     case 'yellow':
-       fgc = '\x1b[33m'
-       break;
-     case 'blue':
-       fgc = '\x1b[34m'
-       break;
-     case 'magenta':
-       fgc = '\x1b[35m'
-       break;
-     case 'cyan':
-       fgc = '\x1b[36m'
-       break;
-     case 'white':
-       fgc = '\x1b[37m'
-       break;
-   }
-   return `${fgc}`
+  let fgc = '\x1b[37m'
+  switch (fg.trim().toLowerCase()) {
+    case 'black':
+      fgc = '\x1b[30m'
+      break;
+    case 'red':
+      fgc = '\x1b[31m'
+      break;
+    case 'green':
+      fgc = '\x1b[32m'
+      break;
+    case 'yellow':
+      fgc = '\x1b[33m'
+      break;
+    case 'blue':
+      fgc = '\x1b[34m'
+      break;
+    case 'magenta':
+      fgc = '\x1b[35m'
+      break;
+    case 'cyan':
+      fgc = '\x1b[36m'
+      break;
+    case 'magenta':
+      fgc = '\x1b[45m'
+      break;
+    case 'white':
+      fgc = '\x1b[37m'
+      break;
+  }
+  return `${fgc}`
 }
 
 const destinationPath = path.join(__dirname, '..', 'logs/');
@@ -51,28 +54,28 @@ async function ensureDirectoryExists(directory) {
 ensureDirectoryExists(destinationPath);
 
 async function logger(log, color = "white") {
-   try {
-      const currentDate = new Date();
-      const formattedDate = currentDate.toISOString().slice(0, 10); // Get YYYY-MM-DD format
+  try {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().slice(0, 10); // Get YYYY-MM-DD format
 
-      const logFileName = `log_${formattedDate}.txt`;
-      const logFilePath = path.join(__dirname, "..", 'logs', logFileName); // Adjust the directory as needed
+    const logFileName = `log_${formattedDate}.txt`;
+    const logFilePath = path.join(__dirname, "..", 'logs', logFileName); // Adjust the directory as needed
 
-      const logMessage = `${currentDate.toISOString()} - ${log}\n`;
+    const logMessage = `${currentDate.toISOString()} - ${log}\n`;
 
-      // Check if the log file for the current date already exists
-      const fileExists = await fs.access(logFilePath).then(() => true).catch(() => false);
+    // Check if the log file for the current date already exists
+    const fileExists = await fs.access(logFilePath).then(() => true).catch(() => false);
 
-      // If the file exists, append to it; otherwise, create a new one
-      if (fileExists) {
-         await fs.appendFile(logFilePath, logMessage);
-      } else {
-         await fs.writeFile(logFilePath, logMessage);
-      }
-      console.log(getColor(color), log, getColor("white"));
-   } catch (err) {
-      console.error('Error writing to log file:', err);
-   }
+    // If the file exists, append to it; otherwise, create a new one
+    if (fileExists) {
+      await fs.appendFile(logFilePath, logMessage);
+    } else {
+      await fs.writeFile(logFilePath, logMessage);
+    }
+    console.log(getColor(color), log, getColor("white"));
+  } catch (err) {
+    console.error('Error writing to log file:', err);
+  }
 }
 
 module.exports = logger
